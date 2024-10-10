3c6ad3794350fb32e664fce34532601cdcc369e4
giriraj <giriraj@Giriraj-Hans>
2024-10-10 14:50:45 +0530

    Second Commit

--- src/p1/Giriraj.java
+++ src/p1/Giriraj.java
@@ -1,8 +1,33 @@
 package p1;
+class Dog {
+	String name;
+	int cost;
+	String breed;
+	
+	void addData(int a,String b, String c) {
+		name=b;
+		cost=a;
+		breed=c;
+	}
+	void display() {
+		System.out.println(name);
+		System.out.println(cost);
+		System.out.println(breed);
+		System.out.println("==============");
+		
+		
+	}
+}
+
+
 
 public class Giriraj {
 
 	public static void main(String[] args) {
+		Dog d=new Dog();
+		d.display();
+		d.addData(39999, "rofckn","JEL");
+		d.display();
 		// TODO Auto-generated method stub
 
 	}
